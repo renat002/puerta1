9fb1b3a1a80a6e92454904e0e00ef0dcd14a1398
usuario@puerta18.corp.ar <usuario@puerta18.corp.ar>
2022-05-13 11:57:26 -0300

    qqq

--- classes/Persona.ts
+++ classes/Persona.ts
@@ -1,4 +1,4 @@
-class Persona{
+export class Persona{
     private nacimiento: number;
     private nombre: string;
 
@@ -6,53 +6,36 @@ class Persona{
        this.nacimiento = anioNacimiento
        this.nombre = nombre;
     };
-public cualEsTuNombre(): string{
+
+    public cualEsTuNombre(): string{
     return "Mi nombre es "+ this.nombre.split(" ")
-};
-public cualEsTuEdad(): string{
+    };
+
+    public cualEsTuEdad(): string{
     return "Mi edad es ";
-}
-public edadActual(){
+    }
+
+    public edadActual(){
         let edad:number = 2022-(this.nacimiento);
     return "Tu edad actual es "+ edad +" años.";
-}
-public cualEsTuGeneracion(){
+    }
+
+    public cualEsTuGeneracion(){
     if (this.nacimiento <1975){
         return "Soy un Baby Boomer";
     }
+
     if (this.nacimiento >1975 && this.nacimiento<1999){
         return "Soy un Milenial";
     } 
+
     if (this.nacimiento >=1999 && this.nacimiento<2010){
         return "Soy un Centenial";
     }
+
     if (this.nacimiento>=2010){
         return "Soy generacion Alfa"
     }
 }
 }
-//termina la def de la clase Persona 
-
-let Miko = new Persona(2018, "Miko Almazan");
-console.log(Miko.cualEsTuGeneracion());
-console.log(Miko.edadActual());
-
-let fede = new Persona(1984, "Federico Fernandez");
-console.log(fede.cualEsTuNombre());
-console.log(fede.cualEsTuGeneracion());
-
-let cadena:string = "Cadena de texto";
-cadena.split
-
-"Otra cadena".split(" ")
-
-
-//Variables de nacimiento 
-const nacimiento=2001;
-
-//Variable de tipo string con mi nombre 
-let nombre:string="Irina";
-
-//Salida 
-console.log("Hola"+nombre+"desde TypeScript");
 
---
+++ index.ts
@@ -0,0 +1,7 @@
+import {Persona} from "./classes/Persona";
+let juanitaa=new Persona (1970, "Juanaa");
+
+let manuelita=new Persona (1998, "Manuela");
+console.log(manuelita.cualEsTuNombre());
+console.log(manuelita.cualEsTuEdad());
+console.log(manuelita.cualEsTuGeneracion())
